d3d107153f8d67e496e56942af06d98ea68c0246
nicksteele <nsteele@duosecurity.com>
2019-02-22 16:41:53 -0500

    Correctly display EC and RSA public keys

--- models/credential.go
+++ models/credential.go
@@ -1,8 +1,20 @@
 package models
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+
+	"github.com/duo-labs/webauthn/protocol"
+
 	"github.com/duo-labs/webauthn/webauthn"
 	"github.com/jinzhu/gorm"
+
+	log "github.com/duo-labs/webauthn.io/logger"
 )
 
 // Credential is the stored credential for Auth
@@ -24,6 +36,64 @@ type Credential struct {
 // the credential.
 func (c *Credential) WebauthnAuthenticator() webauthn.Authenticator {
 	return c.Authenticator.Authenticator
+}
+
+func (c *Credential) DisplayPublicKey() string {
+	parsedKey, err := protocol.ParsePublicKey(c.PublicKey)
+	if err != nil {
+		log.Error("Error parsing the public key bytes:", err)
+		return "Cannot display key"
+	}
+	switch parsedKey.(type) {
+	case protocol.RSAPublicKeyData:
+		pKey := parsedKey.(protocol.RSAPublicKeyData)
+		rKey := &rsa.PublicKey{
+			N: big.NewInt(0).SetBytes(pKey.Modulus),
+			E: int(uint(pKey.Exponent[2]) | uint(pKey.Exponent[1])<<8 | uint(pKey.Exponent[0])<<16),
+		}
+		data, err := x509.MarshalPKIXPublicKey(rKey)
+		if err != nil {
+			log.Error("Error marshalling public key to DER:", err)
+			return "Cannot display key"
+		}
+		pemBytes := pem.EncodeToMemory(&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: data,
+		})
+		return fmt.Sprintf("%s", pemBytes)
+	case protocol.EC2PublicKeyData:
+		pKey := parsedKey.(protocol.EC2PublicKeyData)
+		var curve elliptic.Curve
+		switch pKey.Algorithm {
+		case -7:
+			curve = elliptic.P256()
+		case -35:
+			curve = elliptic.P384()
+		case -36:
+			curve = elliptic.P521()
+		default:
+			log.Error("Error handling curve for EC key")
+			return "Cannot display key"
+		}
+		eKey := &ecdsa.PublicKey{
+			Curve: curve,
+			X:     big.NewInt(0).SetBytes(pKey.XCoord),
+			Y:     big.NewInt(0).SetBytes(pKey.YCoord),
+		}
+		fmt.Printf("Got formatted key %+v\n", eKey)
+		data, err := x509.MarshalPKIXPublicKey(eKey)
+		if err != nil {
+			log.Error("Error marshalling public key to DER:", err)
+			return "Cannot display key"
+		}
+		pemBytes := pem.EncodeToMemory(&pem.Block{
+			Type:  "PUBLIC KEY",
+			Bytes: data,
+		})
+		return fmt.Sprintf("%s", pemBytes)
+	default:
+		return "Cannot display key of this type"
+	}
 }
 
 // CreateCredential creates a new credential object
