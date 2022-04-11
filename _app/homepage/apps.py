debefbebc60b97d077a41d5c58b0dff92c50437b
Matthew Miller <mmiller@duosecurity.com>
2022-04-11 16:00:13 -0700

    Wire up basic homepage

--- _app/homepage/apps.py
+++ _app/homepage/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PasskeysConfig(AppConfig):
+class HomepageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "homepage"
--- _app/homepage/views.py
+++ _app/homepage/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    Render the homepage
+    """
+    return render(request, "homepage/index.html")
