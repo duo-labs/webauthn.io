2c08345c1b20413b94df95da5ecf1edcc03c5953
Matthew Miller <mmiller@duosecurity.com>
2022-05-04 18:46:00 -0700

    Move registration options into service

--- _app/homepage/services/registration.py
+++ _app/homepage/services/registration.py
@@ -1,13 +1,17 @@
-from typing import Union
+from typing import Union, List, Optional
 
 from django.conf import settings
-from webauthn import options_to_json, verify_registration_response
+from webauthn import generate_registration_options, options_to_json, verify_registration_response
+from webauthn.helpers import json_loads_base64url_to_bytes, base64url_to_bytes
 from webauthn.helpers.structs import (
     PublicKeyCredentialCreationOptions,
     RegistrationCredential,
     UserVerificationRequirement,
+    AttestationConveyancePreference,
+    AuthenticatorSelectionCriteria,
+    AuthenticatorAttachment,
+    COSEAlgorithmIdentifier,
 )
-from webauthn.helpers import json_loads_base64url_to_bytes, base64url_to_bytes
 
 from homepage.services import RedisService
 from homepage.exceptions import InvalidRegistrationSession
@@ -18,6 +22,63 @@ class RegistrationService:
 
     def __init__(self):
         self.redis = RedisService(db=2)
+
+    def generate_registration_options(
+        self,
+        *,
+        username: str,
+        attestation: str,
+        attachment: str,
+        require_user_verification: bool,
+        algorithms: List[str],
+    ):
+        _attestation = AttestationConveyancePreference.NONE
+
+        if attestation == "direct":
+            _attestation = AttestationConveyancePreference.DIRECT
+
+        authenticator_selection = AuthenticatorSelectionCriteria(
+            user_verification=UserVerificationRequirement.DISCOURAGED,
+        )
+        if attachment != "all":
+            attachment = AuthenticatorAttachment.CROSS_PLATFORM
+            if attachment == "platform":
+                attachment = AuthenticatorAttachment.PLATFORM
+
+            authenticator_selection.authenticator_attachment = attachment
+
+        if require_user_verification:
+            authenticator_selection.user_verification = UserVerificationRequirement.REQUIRED
+
+        supported_pub_key_algs: Optional[List[COSEAlgorithmIdentifier]] = None
+        if len(algorithms) > 0:
+            supported_pub_key_algs = []
+
+            if "es256" in algorithms:
+                supported_pub_key_algs.append(COSEAlgorithmIdentifier.ECDSA_SHA_256)
+
+            if "rs256" in algorithms:
+                supported_pub_key_algs.append(COSEAlgorithmIdentifier.RSASSA_PKCS1_v1_5_SHA_256)
+
+        registration_options = generate_registration_options(
+            rp_id=settings.RP_ID,
+            rp_name=settings.RP_NAME,
+            user_id=username,
+            user_name=username,
+            attestation=_attestation,
+            authenticator_selection=authenticator_selection,
+            supported_pub_key_algs=supported_pub_key_algs,
+            # TODO: Populate exclude_credentials from existing credentials for the given username?
+        )
+
+        # py_webauthn will default to all supported algorithms on an empty `algorithms` list
+        # so clear it manually so we can test out that scenario
+        if len(algorithms) == 0:
+            registration_options.pub_key_cred_params = []
+
+        self._save_options(username=username, options=registration_options)
+
+        return registration_options
 
     def verify_registration_response(self, *, username: str, credential: RegistrationCredential):
         options = self._get_options(username=username)
@@ -42,7 +103,7 @@ class RegistrationService:
             require_user_verification=require_user_verification,
         )
 
-    def save_options(self, username: str, options: PublicKeyCredentialCreationOptions):
+    def _save_options(self, username: str, options: PublicKeyCredentialCreationOptions):
         """
         Store registration options for the user so we can reference them later
         """
--- _app/homepage/views/registration_options.py
+++ _app/homepage/views/registration_options.py
@@ -1,17 +1,8 @@
 import json
-from typing import List, Optional
 
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
-from webauthn import generate_registration_options, options_to_json
-from webauthn.helpers.structs import (
-    AttestationConveyancePreference,
-    AuthenticatorSelectionCriteria,
-    AuthenticatorAttachment,
-    UserVerificationRequirement,
-    COSEAlgorithmIdentifier,
-)
+from webauthn import options_to_json
 
 from homepage.services import RegistrationService
 from homepage.forms import RegistrationOptionsRequestForm
@@ -26,7 +17,6 @@ def registration_options(request: HttpRequest) -> JsonResponse:
 
     body_json: dict = json.loads(request.body)
 
-    # TODO: Move this all into RegistrationService?
     options_form = RegistrationOptionsRequestForm(body_json)
 
     if not options_form.is_valid():
@@ -39,50 +29,14 @@ def registration_options(request: HttpRequest) -> JsonResponse:
     options_algorithms = form_data["algorithms"]
     options_username = form_data["username"]
 
-    attestation = AttestationConveyancePreference.NONE
-    if options_attestation == "direct":
-        attestation = AttestationConveyancePreference.DIRECT
-
-    authenticator_selection = AuthenticatorSelectionCriteria(
-        user_verification=UserVerificationRequirement.DISCOURAGED,
-    )
-    if options_attachment != "all":
-        attachment = AuthenticatorAttachment.CROSS_PLATFORM
-        if options_attachment == "platform":
-            attachment = AuthenticatorAttachment.PLATFORM
-
-        authenticator_selection.authenticator_attachment = attachment
-
-    if options_require_user_verification:
-        authenticator_selection.user_verification = UserVerificationRequirement.REQUIRED
-
-    supported_pub_key_algs: Optional[List[COSEAlgorithmIdentifier]] = None
-    if len(options_algorithms) > 0:
-        supported_pub_key_algs = []
-
-        if "es256" in options_algorithms:
-            supported_pub_key_algs.append(COSEAlgorithmIdentifier.ECDSA_SHA_256)
-
-        if "rs256" in options_algorithms:
-            supported_pub_key_algs.append(COSEAlgorithmIdentifier.RSASSA_PKCS1_v1_5_SHA_256)
-
-    registration_options = generate_registration_options(
-        rp_id=settings.RP_ID,
-        rp_name=settings.RP_NAME,
-        user_id=options_username,
-        user_name=options_username,
-        attestation=attestation,
-        authenticator_selection=authenticator_selection,
-        supported_pub_key_algs=supported_pub_key_algs,
-        # TODO: Populate exclude_credentials from existing credentials for the given username?
-    )
-
-    # py_webauthn will default to all supported algorithms on an empty `options_algorithms` list
-    # so clear it manually so we can test out that scenario
-    if len(options_algorithms) == 0:
-        registration_options.pub_key_cred_params = []
-
     registration_service = RegistrationService()
-    registration_service.save_options(username=options_username, options=registration_options)
+
+    registration_options = registration_service.generate_registration_options(
+        username=options_username,
+        attachment=options_attachment,
+        attestation=options_attestation,
+        algorithms=options_algorithms,
+        require_user_verification=options_require_user_verification,
+    )
 
     return JsonResponse(json.loads(options_to_json(registration_options)))
