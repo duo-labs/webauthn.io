663467a8a9c3bb8cca983106cccfb9d4c7077037
Matthew Miller <mmiller@duosecurity.com>
2022-05-11 10:54:36 -0700

    Remove profile view for homepage when logged in

--- _app/homepage/views/index.py
+++ _app/homepage/views/index.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from homepage.const import libraries
+from homepage.services import SessionService
 
 
 def index(request):
@@ -11,4 +12,10 @@ def index(request):
         "libraries": libraries,
     }
 
-    return render(request, "homepage/index.html", context)
+    session_service = SessionService()
+
+    template = "homepage/index.html"
+    if session_service.user_is_logged_in(request=request):
+        template = "homepage/profile.html"
+
+    return render(request, template, context)
