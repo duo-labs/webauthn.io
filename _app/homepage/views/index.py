b8abddd6d934c37a576871d6704316eaeb6c2d29
Matthew Miller <mmiller@duosecurity.com>
2022-05-11 16:14:45 -0700

    Revamp profile credentials list

--- _app/homepage/views/index.py
+++ _app/homepage/views/index.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 from homepage.const import libraries
-from homepage.services import SessionService
+from homepage.services import SessionService, CredentialService
+from homepage.helpers import transports_to_ui_string, truncate_credential_id_to_ui_string
 
 
 def index(request):
@@ -17,5 +18,20 @@ def index(request):
     template = "homepage/index.html"
     if session_service.user_is_logged_in(request=request):
         template = "homepage/profile.html"
+
+        username = request.session["username"]
+        credential_service = CredentialService()
+
+        user_credentials = credential_service.retrieve_credentials_by_username(username=username)
+
+        context["credentials"] = [
+            {
+                "id": truncate_credential_id_to_ui_string(cred.id),
+                "sign_count": cred.sign_count,
+                "is_disc_cred": cred.is_discoverable_credential,
+                "transports": transports_to_ui_string(cred.transports or []),
+            }
+            for cred in user_credentials
+        ]
 
     return render(request, template, context)
