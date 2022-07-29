54d90ae8a316f95bd703f897ceeb870183856654
Matthew Miller <mmiller@duosecurity.com>
2022-07-29 16:42:20 -0700

    Clear everything out

--- server/assertion_test.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import "net/http/httptest"
-
-func (ss *ServerSuite) TestGetAssertion() {
-	req := httptest.NewRequest("POST", "/assertion", nil)
-	response := httptest.NewRecorder()
-	ss.server.GetAssertion(response, req)
-}
