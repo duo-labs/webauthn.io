03f21b9998cfbc0c6a7ce44dd46ac3ea080c050a
Matthew Miller <mmiller@duosecurity.com>
2022-08-17 14:01:30 -0700

    Remove manual SCSS process files

--- process-scss.sh
+++
@@ -1,3 +0,0 @@
-# Using Dart-sass CLI from https://sass-lang.com/install
-./sass-1.50.1 --style compressed _app/homepage/static/scss/index.scss _app/homepage/static/css/index.css
-docker compose run django ./manage.py collectstatic --no-input
