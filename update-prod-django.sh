d8eb22b10cb92963369bebce67310cbdd8245e8d
Matthew Miller <mmiller@duosecurity.com>
2022-09-23 12:53:49 -0700

    Update remaining references to Nginx

--- update-prod-django.sh
+++ update-prod-django.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "[START]"
-echo "---stopping django and nginx---"
-docker-compose stop django nginx
+echo "---stopping django and caddy---"
+docker-compose stop django caddy
 echo "---removing containers---"
 docker-compose rm -f
 echo "---removing stale volumes---"
 docker volume prune -f
 echo "---rebuilding django---"
 docker-compose build django
-echo "---restarting django and nginx---"
-docker-compose -f docker-compose.yml up -d --no-deps django nginx
+echo "---restarting django and caddy---"
+docker-compose -f docker-compose.yml up -d --no-deps django caddy
 echo "[END]"
