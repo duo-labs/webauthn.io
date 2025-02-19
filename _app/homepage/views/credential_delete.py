66d6deab28f6e7f2d3b4e1ca42f6ac1a0358433e
Matthew Miller <mmiller@duosecurity.com>
2025-02-19 14:37:09 -0800

    Make cred delete endpoint return JSON

--- _app/homepage/views/credential_delete.py
+++ _app/homepage/views/credential_delete.py
@@ -1,11 +1,11 @@
-from django.shortcuts import redirect
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 
 from homepage.services import CredentialService
 
 
 @require_http_methods(["POST"])
-def credential_delete(request, credential_id):
+def credential_delete(_request, credential_id):
     credential_service = CredentialService()
     credential_service.delete_credential_by_id(credential_id=credential_id)
-    return redirect("index")
+    return JsonResponse({"deleted": True})
