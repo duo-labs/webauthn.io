6abe18871bac8d38848b704b262ee3cdd9189928
Matthew Miller <mmiller@duosecurity.com>
2025-09-29 15:13:55 -0700

    Tweak some var names in tests

--- _app/homepage/tests/test_view_authentication_options.py
+++ _app/homepage/tests/test_view_authentication_options.py
@@ -24,14 +24,14 @@ class TestViewAuthenticationOptions(TestCase):
         self.assertEqual(options_json.get("hints"), ["security-key", "hybrid", "client-device"])
 
     def test_returns_400_bad_argument(self):
-        options = self.client.post(
+        response = self.client.post(
             self.route,
             {},
         )
 
-        options_json = options.json()
+        options_json = response.json()
 
-        self.assertEquals(options.status_code, HttpResponseBadRequest.status_code)
+        self.assertEquals(response.status_code, HttpResponseBadRequest.status_code)
         self.assertEquals(
             options_json,
             {"error": "Could not parse request: Expecting value: line 1 column 1 (char 0)"},
--- _app/homepage/tests/test_view_registration_response.py
+++ _app/homepage/tests/test_view_registration_response.py
@@ -9,14 +9,14 @@ class TestViewRegistrationVerification(TestCase):
         self.client.get(reverse("index"))
 
     def test_returns_400_bad_argument(self):
-        options = self.client.post(
+        response = self.client.post(
             self.route,
             {},
         )
 
-        options_json = options.json()
+        options_json = response.json()
 
-        self.assertEquals(options.status_code, HttpResponseBadRequest.status_code)
+        self.assertEquals(response.status_code, HttpResponseBadRequest.status_code)
         self.assertEquals(
             options_json,
             {"error": "Could not parse request: Expecting value: line 1 column 1 (char 0)"},
