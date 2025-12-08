d919706895c98f3c618d004b206f7756e9ce3b66
daminoux <54556803+daminoux43@users.noreply.github.com>
2025-12-08 15:17:38 +0100

    Update start-prod.sh fix new version docker compose

--- start-prod.sh
+++ start-prod.sh
@@ -1,1 +1,10 @@
-docker-compose -f docker-compose.yml up -d
+if command -v docker-compose &> /dev/null; then
+    COMPOSE_CMD="docker-compose"
+elif docker compose version &> /dev/null; then
+    COMPOSE_CMD="docker compose"
+else
+    echo "Erreur : ni 'docker-compose' ni 'docker compose' n'est disponible."
+    exit 1
+fi
+
+$COMPOSE_CMD -f docker-compose.yml up -d
